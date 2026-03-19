18526720a7c730bd965a8b2ae5ffc55cec11f6b8
csoftwaree <leonardopoeiras@hotmail.com>
2026-03-18 23:47:56 -0300

    .env ajustadp + main;py

--- app/api/whatsapp_webhook.py
+++ app/api/whatsapp_webhook.py
@@ -1,18 +1,23 @@
+import os
 from fastapi import APIRouter, Request
 
 router = APIRouter()
 
-VERIFY_TOKEN = "meu_token_verificacao"
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+
 
 @router.get("/webhook")
 async def verify(request: Request):
     params = request.query_params
 
-    if (
-        params.get("hub.mode") == "subscribe"
-        and params.get("hub.verify_token") == VERIFY_TOKEN
-    ):
-        return int(params.get("hub.challenge"))
+    mode = params.get("hub.mode")
+    token = params.get("hub.verify_token")
+    challenge = params.get("hub.challenge")
+
+    print("DEBUG:", mode, token, challenge)
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        return int(challenge)
 
     return {"error": "Verification failed"}
 
@@ -20,6 +25,6 @@ async def verify(request: Request):
 @router.post("/webhook")
 async def receive_message(request: Request):
     body = await request.json()
-    print("Mensagem recebida:", body)
+    print("📩 Mensagem recebida:", body)
 
     return {"status": "ok"}
